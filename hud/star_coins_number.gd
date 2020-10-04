8c70edeab6915b3db11862f1170f0e3959f1ee29
Noah Bumgardner <noah@playgamecafe.com>
2020-10-03 22:34:48 -0500

    Keep Build, Readme, and License Files

--- hud/star_coins_number.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-
-onready var player_variables = get_node("/root/PlayerVariables")
-
-
-func _process(_delta):
-	text = str(player_variables.player_currency_star_coin)
--- main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-onready var player_variables = get_node("/root/PlayerVariables")
-
-
-func _on_Player_set_star_coin_increase(amount):
-	player_variables.player_currency_star_coin += amount
