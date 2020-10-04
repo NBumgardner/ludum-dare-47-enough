728ea3d66f8fdfa3271dd1dd289c48b020212325
Noah Bumgardner <noah@playgamecafe.com>
2020-10-04 16:31:03 -0500

    Use Game Over Method to Check for Text Display

--- hud/game_over_text.gd
+++ hud/game_over_text.gd
@@ -1,17 +1,8 @@
 extends Label
 
 
-const GAME_OVER_LOSE = "Game Over"
-const GAME_OVER_STAR_COIN_AMOUNT = 0
-const GAME_OVER_WIN = "Congratulations!"
-const GAME_WIN_STAR_COIN_AMOUNT = 10
-
-
 onready var game_over_conditions = get_node("/root/GameOverConditions")
 
 
 func _process(_delta):
-	if game_over_conditions.is_player_rich():
-		text = game_over_conditions.GAME_OVER_WIN_TEXT
-	elif game_over_conditions.is_player_out_of_money():
-		text = game_over_conditions.GAME_OVER_LOSE_TEXT
+	text = game_over_conditions.get_game_over_text()
