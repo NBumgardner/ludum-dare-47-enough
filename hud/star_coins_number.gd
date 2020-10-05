c1026d4586735b742889cba2c483b36de848c2f8
Noah Bumgardner <noah@playgamecafe.com>
2020-10-05 10:51:11 -0500

    Show Star Coin Win Condition on HUD

--- hud/star_coins_number.gd
+++ hud/star_coins_number.gd
@@ -1,8 +1,21 @@
 extends Label
 
 
+onready var game_over_conditions = get_node("/root/GameOverConditions")
 onready var player_variables = get_node("/root/PlayerVariables")
 
 
 func _process(_delta):
-	text = str(player_variables.player_currency_star_coin)
+	text = _get_star_coin_amount_and_goal()
+
+func _get_star_coin_amount_and_goal():
+	var amount = str(player_variables.player_currency_star_coin)
+	var goal = ""
+
+	if game_over_conditions.GAME_OVER_WIN_STAR_COIN_AMOUNT > 0:
+		goal = (
+			" / "
+			+ str(game_over_conditions.GAME_OVER_WIN_STAR_COIN_AMOUNT)
+		)
+
+	return amount + goal
